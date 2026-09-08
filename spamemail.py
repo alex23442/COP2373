360c9e442fd15cf7ecacbe4b2a48d7423b23bce8
yeser <calel9334@gmail.com>
2026-09-07 22:33:40 -0400

    Added a dictonary for the spam words. Added both interface 3 and 4. Got started with the accumalator.
    Signed-off-by: yeser <calel9334@gmail.com>

--- spamemail.py
+++ spamemail.py
@@ -1,8 +1,43 @@
+#TODO add comments, finish all interfaces.
 import customtkinter as ctk
 
 app = ctk.CTk()
 
 app.geometry("400x400")
+
+spam_words = {
+    "money": 5,
+    "fast": 5,
+    "guaranteed": 10,
+    "returns": 5,
+    "profit": 5,
+    "double": 5,
+    "income": 5,
+    "rich": 5,
+    "million": 10,
+    "dollars": 5,
+    "cash": 5,
+    "financial": 5,
+    "freedom": 5,
+    "act": 5,
+    "now": 5,
+    "last": 5,
+    "chance": 5,
+    "immediate": 5,
+    "response": 5,
+    "required": 5,
+    "limited": 5,
+    "time": 5,
+    "hurry": 5,
+    "expires": 5,
+    "today": 5,
+    "final": 5,
+    "call": 5,
+    "free": 10,
+    "winner": 10,
+    "unbelievable": 10
+}
+
 
 def interface1():
 
@@ -10,7 +45,7 @@ def interface1():
 
     frame.pack(fill="both", expand=True)
 
-    label = ctk.CTkLabel(frame, text="Hello World")
+    label = ctk.CTkLabel(frame, text="Do you want to send a message?")
 
     label.pack(pady=30)
 
@@ -32,7 +67,7 @@ def interface2():
     frame = ctk.CTkFrame(app)
     frame.pack(fill="both", expand=True)
 
-    label = ctk.CTkLabel(frame, text="Hello ")
+    label = ctk.CTkLabel(frame, text="Please enter your email.")
 
     label.pack(pady=30)
 
@@ -42,9 +77,48 @@ def interface2():
 
     enter_button = ctk.CTkButton(frame,
                                  text="Enter",
-                                 command=frame.destroy)
+                                 command=lambda: interface3(user_email, frame))
 
     enter_button.pack(side="right", pady=10, padx=25)
+
+def interface3(user_email,frame):
+
+    useer = user_email.get()
+
+    frame.destroy()
+
+    frame = ctk.CTkFrame(app)
+    frame.pack(fill="both", expand=True)
+
+    label = ctk.CTkLabel(frame, text=useer)
+
+    label.pack(pady=30)
+
+    user_massage = ctk.CTkEntry(frame)
+
+    user_massage.pack(side="bottom", pady=10, padx=25)
+
+    command = interface4(user_massage)
+
+def interface4(user_massage):
+    frame = ctk.CTkFrame(app)
+    frame.pack(fill="both", expand=True)
+
+    user_massage = user_massage.get()
+
+    score = 0
+
+    while True:
+        if user_massage == spam_words.get(user_massage):
+            score += points
+
+            print(score)
+
+            label = ctk.CTkLabel(frame, text=score)
+
+            label.pack(pady=30)
+
+
 
 
 
